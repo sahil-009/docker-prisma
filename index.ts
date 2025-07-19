3bb1eb66a171493ca0d80cf1cbcb9c15b28d8d31
sahil-009 <sahilmund.6685@gmail.com>
2025-07-20 00:30:39 +0530

    ps auth

--- index.ts
+++ index.ts
@@ -1,14 +1,26 @@
-const express =require ("express");
-const app = express()
+import { PrismaClient } from "@prisma/client";
 
+const express =require ("express");
+
+
+
+const app = express()
+const prismaClient = new PrismaClient();
 
 app.get("/" ,(req,res)=>{
+   
 	res.json({
 	"message":"Get:endpoint"
 	})
 })
 
-app.post("/" ,(req,res)=>{
+const data  = app.post ("/" ,async(req,res)=>{
+    await prismaClient.user.create({
+        data:{
+            username:Math.random().toString(),
+            password:Math.random().toString()
+        }
+    })
 	res.json({
 	"message":"Post:endpoint"
 	})
